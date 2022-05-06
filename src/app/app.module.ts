839cd4947c4c94b277ba8962a1dfab893a5f8243
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-05-05 22:36:49 -0300

    Two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
 import { DataBidingComponent } from './data-biding/data-biding.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DataBidingComponent } from './data-biding/data-biding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

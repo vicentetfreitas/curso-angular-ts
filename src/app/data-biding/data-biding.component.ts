5707fe67109e6fd3338c4cf2eb14eddcfdc2813c
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-05-05 21:33:23 -0300

    Interpolation

---
+++ src/app/data-biding/data-biding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-biding',
+  templateUrl: './data-biding.component.html',
+  styleUrls: ['./data-biding.component.scss']
+})
+export class DataBidingComponent implements OnInit {
+ public nome: string = "Vicente";
+ public idade: number = 30;
+ public maisUm: number = 1;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

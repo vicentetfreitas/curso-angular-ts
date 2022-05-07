77466e75b413db2c78fffc5195358e36fec88559
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-05-07 18:39:06 -0300

    @Input()

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { InputComponent } from './input/input.component';
 import { NewComponentComponent } from './new-component/new-component.component';
 
 
 
 @NgModule({
   declarations: [
-    NewComponentComponent
+    NewComponentComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    NewComponentComponent
+    NewComponentComponent, InputComponent
   ]
 })
 export class SharedModule { }

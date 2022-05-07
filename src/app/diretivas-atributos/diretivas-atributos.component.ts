2aebc48adb0a98dcfe585a32ad0af5e9389dafd0
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-05-07 17:05:34 -0300

    [ngStyle]

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -3,21 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretivas-atributos',
   templateUrl: './diretivas-atributos.component.html',
-  styleUrls: ['./diretivas-atributos.component.scss']
+  styleUrls: ['./diretivas-atributos.component.scss'],
 })
 export class DiretivasAtributosComponent implements OnInit {
-
   public valor: boolean = true;
-
-  constructor() { }
+  public heightPx: string = "20px";
+  public backgroundColor: string = "tomato";
+  constructor() {}
 
   ngOnInit(): void {
     setInterval(() => {
-      if(this.valor){
+      if (this.valor) {
         this.valor = false;
-      }else{
+      } else {
         this.valor = true;
       }
-    },2000)
+      if (this.heightPx == "20px") {
+        this.heightPx = '50px';
+        this.backgroundColor = "cornflowerblue";
+      } else {
+        this.heightPx = "20px";
+        this.backgroundColor = "tomato"
+      }
+    }, 2000);
   }
 }
